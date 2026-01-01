8c52ff34e377a6ff65768f440ee1a46432889a6b
Angelicale <gianghgle@gmail.com>
2026-01-01 21:42:59 +0000

    updated favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
-// import Introduction from "../components/Introduction";
 import Review from "../components/Review";
 import BambooExperience from "@/components/Experience";
 import BeforeYouVisit from "@/components/BeforeYouVisit";
 
 export const metadata = {
-  title: "Nails as Art",
+  title: "Bamboo Nails & Spa Hull - Nails as Art",
   description:
     "Professional nail salon in nail salon in Anlaby Road, Hull specialising in BIAB, gel nails and modern artistic designs.",
 };
